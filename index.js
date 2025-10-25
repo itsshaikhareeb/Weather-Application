83583200cf36412293dc072f8f3fd0357a8c266c
Shaikh Areeb <shaikhareeb321@gmail.com>
2025-10-25 19:37:23 +0530

    add clicking functionality to search button

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const btn = document.getElementById("btnSearch")
 const inpt = document.getElementById("inptSearch")
 const API_KEY = 'a8e34201d7793aff0619f1563c184630'
 const info = document.getElementById("extraInfo")
+let res= ""
+// let hold = ""
 
-async function fetchData(city_name) {
-    let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`)
-    let res = await result.json()
-    console.log(res.name);
+
+
+
+
+ async function fetchData(city_name) {
+    if(city_name == ""){
+        emptyInpt()
+        return;
+    }
+     let result = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_KEY}&units=metric`)
+     res = await result.json()
+    // console.log(res.message);
+    console.log(`this is ${res}`);
+    if( res == "" || res.cod == "404" || res.cod == "400"){
+        emptyInpt()
+        return;
+    }
     info.innerHTML = `
                 <div  id="temp">
                   <p id='temp1' >${res.main.temp}°C</p> 
@@ -20,9 +35,18 @@ async function fetchData(city_name) {
 }
 
 btn.addEventListener('click', ()=>{
-    let nameOfCity = inpt.value
-    fetchData(nameOfCity)
-    
-   
-
+    fetchData(inpt.value)
 })
+
+inpt.addEventListener('keydown', (e)=>{
+    if(e.key === "Enter"){
+        console.log(res);
+        fetchData(inpt.value)
+
+    }
+})
+
+function emptyInpt(){
+    alert("Please enter valid city name.")
+}
+
